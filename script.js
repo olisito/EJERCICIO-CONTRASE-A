b7408cad7dab107df60cd6b0c2a58e077194e226
david_oliver <daviid_oliver@hotmail.com>
2025-02-06 13:46:25 +0100

    finalizado

---
+++ script.js
@@ -0,0 +1,18 @@
+const input = document.getElementById("input");
+const parrafo = document.getElementById("parrafo");
+function contraseña() {
+    const longitud = input.value.length;
+    if (longitud <= 8) {
+        parrafo.innerHTML = "contraseña NO segura";
+        parrafo.setAttribute("class", "rojo");
+    } else if (longitud > 8 && longitud <= 10) {
+        parrafo.innerHTML = "contraseña CASI segura";
+        parrafo.setAttribute("class", "azul");
+    } else {
+        parrafo.innerHTML = "contraseña MUY segura";
+
+        parrafo.setAttribute("class", "verde");
+    }
+}
+
+input.addEventListener("keyup", contraseña);
