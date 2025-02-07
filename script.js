48700906766cbd302327ace38bb5e4c5a7e808ba
Jaime López <jaimelopezmislata@gmail.com>
2025-02-07 13:25:17 +0100

    Login v1.0 (#1)
    * Mejoras javaScript y estilos
    
    * Funcionalidad con login
    
    * Mejoras en validaciones

--- script.js
+++ script.js
@@ -1,18 +1,43 @@
-const input = document.getElementById("input");
+const inputUser = document.getElementById("inputUser");
+const inputPass = document.getElementById("inputPass");
 const parrafo = document.getElementById("parrafo");
-function contraseña() {
-    const longitud = input.value.length;
-    if (longitud <= 8) {
-        parrafo.innerHTML = "contraseña NO segura";
-        parrafo.setAttribute("class", "rojo");
-    } else if (longitud > 8 && longitud <= 11) {
-        parrafo.innerHTML = "contraseña CASI segura";
-        parrafo.setAttribute("class", "azul");
-    } else {
-        parrafo.innerHTML = "contraseña MUY segura";
+const button = document.getElementById("button");
 
+function contraseña() {
+    const longitud = inputPass.value.length;
+    if (inputUser.value.length == 0 && inputPass.value.length == 0) {
+        parrafo.textContent = "";
+    } else if (longitud == 0){
+        parrafo.textContent = "Contraseña requerida";
+        parrafo.setAttribute("class", "rojo");
+        button.disabled = true;
+    } else if (inputUser.value.length == 0){
+        parrafo.textContent = "Usuario requerido";
+        parrafo.setAttribute("class", "rojo");
+        button.disabled = true;
+    } else if (longitud > 0 && longitud <= 8) {
+        parrafo.textContent = "Contraseña NO segura";
+        parrafo.setAttribute("class", "rojo");
+        button.disabled = true;
+    } else if (longitud > 8 && longitud <= 11) {
+        parrafo.textContent = "Contraseña CASI segura";
+        parrafo.setAttribute("class", "azul");
+        button.disabled = true;
+    } else if (longitud > 11){
+        parrafo.textContent = "Contraseña MUY segura";
         parrafo.setAttribute("class", "verde");
+        if (inputUser.value.length != 0) {
+            button.disabled = false;
+        } else {
+            button.disabled = true;
+        }
     }
 }
 
-input.addEventListener("keyup", contraseña);
+function mostrarUsuario() {
+    alert("HOLA " + inputUser.value);
+}
+
+button.addEventListener("click", mostrarUsuario);
+inputPass.addEventListener("keyup", contraseña);
+inputUser.addEventListener("keyup", contraseña);
