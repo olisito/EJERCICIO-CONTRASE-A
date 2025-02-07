d036a37e7d1bf564d3e5f92f64d75d6f4811d1e3
jaime-lopez <jaimelopezmislata@gmail.com>
2025-02-07 10:05:05 +0100

    Mejoras javaScript y estilos

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ const parrafo = document.getElementById("parrafo");
 function contraseña() {
     const longitud = input.value.length;
     if (longitud <= 8) {
-        parrafo.innerHTML = "contraseña NO segura";
+        parrafo.textContent = "contraseña NO segura";
         parrafo.setAttribute("class", "rojo");
     } else if (longitud > 8 && longitud <= 11) {
-        parrafo.innerHTML = "contraseña CASI segura";
+        parrafo.textContent = "contraseña CASI segura";
         parrafo.setAttribute("class", "azul");
     } else {
-        parrafo.innerHTML = "contraseña MUY segura";
+        parrafo.textContent = "contraseña MUY segura";
 
         parrafo.setAttribute("class", "verde");
     }
