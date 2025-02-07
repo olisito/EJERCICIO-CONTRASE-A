0660384d4b1745d463cec37dc99e69d6a9e62849
jaime-lopez <jaimelopezmislata@gmail.com>
2025-02-07 13:10:28 +0100

    Mejoras en validaciones

--- script.js
+++ script.js
@@ -8,17 +8,23 @@ function contraseña() {
     if (inputUser.value.length == 0 && inputPass.value.length == 0) {
         parrafo.textContent = "";
     } else if (longitud == 0){
-        parrafo.textContent = "";
-    } else if (longitud <= 8) {
-        parrafo.textContent = "contraseña NO segura";
+        parrafo.textContent = "Contraseña requerida";
+        parrafo.setAttribute("class", "rojo");
+        button.disabled = true;
+    } else if (inputUser.value.length == 0){
+        parrafo.textContent = "Usuario requerido";
+        parrafo.setAttribute("class", "rojo");
+        button.disabled = true;
+    } else if (longitud > 0 && longitud <= 8) {
+        parrafo.textContent = "Contraseña NO segura";
         parrafo.setAttribute("class", "rojo");
         button.disabled = true;
     } else if (longitud > 8 && longitud <= 11) {
-        parrafo.textContent = "contraseña CASI segura";
+        parrafo.textContent = "Contraseña CASI segura";
         parrafo.setAttribute("class", "azul");
         button.disabled = true;
     } else if (longitud > 11){
-        parrafo.textContent = "contraseña MUY segura";
+        parrafo.textContent = "Contraseña MUY segura";
         parrafo.setAttribute("class", "verde");
         if (inputUser.value.length != 0) {
             button.disabled = false;
